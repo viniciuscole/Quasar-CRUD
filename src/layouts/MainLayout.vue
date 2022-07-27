dbfa7bfa104e182307f786c3229d91855965db5b
viniciuscole <viniamorim87@hotmail.com>
2022-07-27 12:43:39 -0300

    minor changes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -13,7 +13,7 @@
 
         <q-toolbar-title> UFES - CURSOS </q-toolbar-title>
 
-        <div><img src="../assets/logo.png" alt="UFES-LOGO"></div>
+        <div><img src="../assets/logo.png" alt="UFES-LOGO" /></div>
       </q-toolbar>
     </q-header>
 
@@ -21,7 +21,7 @@
       <q-list>
         <q-item-label header> Menus </q-item-label>
 
-        <EssentialLink 
+        <EssentialLink
           style="color: whitesmoke"
           v-for="link in essentialLinks"
           :key="link.title"
@@ -36,33 +36,56 @@
 
     <q-footer elevated class="footer">
       <q-toolbar>
+        <q-space />
         <span class="title">
-            Universidade Federal do Espírito Santo
-            <span class="sub-title">
-                <span>
-                    <img class="f-icon" src="../assets/fonte-de-icone-de-marker-gratis.png" alt="icone-localização">
-                    Av. Fernando Ferrari, 514 - Goiabeiras, Vitória - ES, 29075-910
-                </span>
+          Universidade Federal do Espírito Santo
+          <span class="sub-title">
+            <span>
+              <img
+                class="f-icon"
+                src="../assets/fonte-de-icone-de-marker-gratis.png"
+                alt="icone-localização"
+              />
+              Av. Fernando Ferrari, 514 - Goiabeiras, Vitória - ES, 29075-910
             </span>
+          </span>
         </span>
-        <q-space/>
+        <q-space />
+        <q-space />
         <span class="title">
-            Contatos
-            <span class="sub-title">
-                <span>
-                    <img class="f-icon" src="../assets/fonte-de-icone-de-phone-call-gratis.png" alt="icone-telefone">
-                    (27) 4009-2222
-                </span>
-                <span>
-                    <img class="f-icon" src="../assets/fonte-de-icone-de-facebook-gratis.png" alt="icone-facebook">
-                    <a href="https://pt-br.facebook.com/ufesoficial/" target="_blank">  facebook.com/ufesoficial</a>
-                </span>
-                <span>
-                    <img class="f-icon" src="../assets/fonte-de-icone-de-youtube-gratis.png" alt="icone-youtube">
-                    <a href="https://www.youtube.com/ufesoficial" target="_blank">  youtube.com/ufesoficial</a>
-                </span>
+          Contatos
+          <span class="sub-title">
+            <span>
+              <img
+                class="f-icon"
+                src="../assets/fonte-de-icone-de-phone-call-gratis.png"
+                alt="icone-telefone"
+              />
+              (27) 4009-2222
             </span>
+            <span>
+              <img
+                class="f-icon"
+                src="../assets/fonte-de-icone-de-facebook-gratis.png"
+                alt="icone-facebook"
+              />
+              <a href="https://pt-br.facebook.com/ufesoficial/" target="_blank">
+                facebook.com/ufesoficial</a
+              >
+            </span>
+            <span>
+              <img
+                class="f-icon"
+                src="../assets/fonte-de-icone-de-youtube-gratis.png"
+                alt="icone-youtube"
+              />
+              <a href="https://www.youtube.com/ufesoficial" target="_blank">
+                youtube.com/ufesoficial</a
+              >
+            </span>
+          </span>
         </span>
+        <q-space />
       </q-toolbar>
     </q-footer>
   </q-layout>
@@ -102,40 +125,41 @@ export default defineComponent({
 });
 </script>
 <style>
-  .header, .menu{
-    background-color: rgba(0, 0, 46, 0.979);
-  }
-  .drawer{
-    background-color: rgb(0, 0, 29);
-  }
-  .header img{
-    height: 40px;
-  }
-  .footer{
-    padding: 25px 0px;
-    background-color: rgba(0, 0, 46, 0.979);
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-    height: 100px;
-    color: whitesmoke;
-    align-items: center;
-  }
-  .title{
-    display: block;
-    font-size: 16px;
-    text-align: center;
-  }
-  a{
-    text-decoration: none;
-    color: whitesmoke;
-  }
-  .sub-title{
-    text-align: left;
-    display: flex;
-    flex-direction: column;
-  }
-  .f-icon{
-    height: 15px;
-  }
+.header,
+.menu {
+  background-color: rgba(0, 0, 46, 0.979);
+}
+.drawer {
+  background-color: rgb(0, 0, 29);
+}
+.header img {
+  height: 40px;
+}
+.footer {
+  padding: 25px 0px;
+  background-color: rgba(0, 0, 46, 0.979);
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  height: 100px;
+  color: whitesmoke;
+  align-items: center;
+}
+.title {
+  display: block;
+  font-size: 16px;
+  text-align: center;
+}
+a {
+  text-decoration: none;
+  color: whitesmoke;
+}
+.sub-title {
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+}
+.f-icon {
+  height: 15px;
+}
 </style>
